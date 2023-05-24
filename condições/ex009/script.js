2ef05eb6ea44a8b73d2bc6b1b6fabee3050fbae2
Marcosfvs <129077080+Marcosfvs@users.noreply.github.com>
2023-05-24 19:32:16 -0300

    horadodia

--- condições/ex009/script.js
+++ condições/ex009/script.js
@@ -1,1 +1,19 @@
-alert('olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()  
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora <12) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background ='#fee060'
+        
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#63442f'
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#2e3b4d'
+    }
+}
