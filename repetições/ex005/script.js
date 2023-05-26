4fe5021a5993e1354d70c39a6512162c7dad8b37
Marcosfvs <129077080+Marcosfvs@users.noreply.github.com>
2023-05-26 12:14:27 -0300

    exe2

--- repetições/ex005/script.js
+++ repetições/ex005/script.js
@@ -1,11 +1,24 @@
 function calcular() {
 
-    var n = document.getElementById('num')
-    var res = document.getElementById('res')
-    var a = 1
-    
-    for (a ; a < 11; a++) {
-    var s = a * n
-    res.innerHTML = (`${a.value} * ${n.value} = ${s.value}`)
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+   //teste se o input ta vazio " cuidado ao escrever length"
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+        // se não vazio converte a string num em um número e cria a variavel c
+    } else {        
+        let n = Number(num.value)
+        let c = 1
+        // antes de pedir um novo numero deixa o select vazio
+        tab.innerHTML = ''
+        //repetição com option, vai entrar o select
+        for (c ; c < 11; c++) {
+            //cria a variavel item para adicionar ao option no select
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value =`tab${c}`
+            //adiciona o item ao option no select
+            tab.appendChild(item)  
+        }         
     }
 }
