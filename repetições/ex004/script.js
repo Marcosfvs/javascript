252a3ee7a7d8e56e38bdd7255f1e0dd81f6edbd1
Marcosfvs <129077080+Marcosfvs@users.noreply.github.com>
2023-05-26 11:54:03 -0300

    exe1

--- repetições/ex004/script.js
+++ repetições/ex004/script.js
@@ -1,12 +1,39 @@
 function contar() {
-    var sinc = document.getElementById('inc')
-    var sfim = document.getElementById('fim')
-    var spass = document.getElementById('pass')
-    var sres = document.getElementById('res')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    for (sinc; sinc <= sfim; sinc = sinc + spass) {
-        sres.innerHTML = `${sinc.value}`
-    }     
+    // teste de se o input está vazio
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar'
+        //window.alert('[ERRO] faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        //mconverter o input para number
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        //Se o passo for zero trava o navegador, considerou como 1 nesse caso
+        if (p<= 0) {
+            window.alert('Passo inválido! Considerando PASSO = 1')
+            p = 1
+        }
+        if (i <f) {
+            //contagem crescente
+            for(let c = i; c <=f; c+= p){
+            res.innerHTML += `${c} 👉 ` // usei o windows + . para achar esse emote
+            }
+            res.innerHTML += '\u{1F3C1}'
+        } else{
+            //contagem regressiva
+            for(let c= i; c >=f; c -= p) {
+                res.innerHTML += `${c} 👉 `
+            }
+            res.innerHTML += '\u{1F3C1}'
+        }
+    }  
 }
+
 
 
